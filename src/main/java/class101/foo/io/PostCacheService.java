428ff7fc1928489c416ef4e482c02908316c399f
destiny3912 <destiny3910@gmail.com>
2024-02-18 04:08:35 +0900

    feat: 캐싱

---
+++ src/main/java/class101/foo/io/PostCacheService.java
@@ -0,0 +1,29 @@
+package class101.foo.io;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class PostCacheService {
+    private final PostRepository postRepository;
+
+    private Page<Post> firstPostPage;
+
+    // 1초에 한번씩 실행
+    @Scheduled(cron = "* * * * * *")
+    public void updateFirstPostPage() {
+
+        firstPostPage = postRepository.findAll(
+                PageRequest.of(0, 20, Sort.by("id").descending())
+        );
+    }
+
+    public Page<Post> getFirstPostPage() {
+        return this.firstPostPage;
+    }
+}
